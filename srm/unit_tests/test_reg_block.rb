88d8143a066e7f5529d6241ff3e66f4d3870b7a4
sanjeev singh <snjvsingh123@gmail.com>
2017-09-16 18:09:40 -0400

    added json to block

--- srm/unit_tests/test_reg_block.rb
+++ srm/unit_tests/test_reg_block.rb
@@ -18,5 +18,15 @@ include SRM
     assert_equal("cpu_registers", @reg_block.name)
   end
 
+  def test_json
+    json_string = @reg_block.to_json
+    h = JSON.parse(json_string, object_class: OpenStruct)
+    assert_equal "Block", h.type
+    assert_equal "cpu_registers", h.name
+    assert_equal "r1", h.nodes[0].name
+    assert_equal [0x100, 0x32], h.nodes[0].offsets["cpu_map"]
+    assert_equal [0x200, 0x16], h.nodes[0].offsets["gpu_map"]
+  end
+
 end
 
