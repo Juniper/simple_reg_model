36a571b3fea1468426371402cdbfe8bebe4a1687
sanjeev singh <snjvsingh123@gmail.com>
2017-09-17 10:48:46 -0400

    all tests pass

--- srm/unit_tests/test_block_json.rb
+++ srm/unit_tests/test_block_json.rb
@@ -10,7 +10,7 @@ RegBlock.new(name: "cpu_reg32") do |model|
   model << SRM::Register.new(name: "r1_reg") do |r|
                   r << SRM::Field.new(name: "field", nbits: 32, lsb_pos: 0, volatile: false, 
                                  hard: 0xdeadbeef, mbist: 0x0)
-            end
+            end.offset(cpu_map: [0x100, 0x32], gpu_map: [0x200, 0x16])
 end
 )
   end
@@ -21,6 +21,10 @@ end
     assert_equal "r1_reg", r1.nodes[0].name
     assert_equal SRM::RegBlock, r1.class
     assert_equal SRM::Register, r1.nodes[0].class
+
+    assert_equal "field", r1.nodes[0].fields[0].name
+    assert_equal 32, r1.nodes[0].fields[0].nbits
+    assert_equal [0x100, 50], r1.nodes[0].offsets[:cpu_map]
   end
 
 end
